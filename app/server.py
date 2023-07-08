4a86ec672986f95eb63585eac02cb95809a52aa9
andressa.magalhaes <andressa.ribeiro@wp090383.local>
2023-07-08 15:05:54 -0300

    feat: added is_universal query string parameter

--- app/server.py
+++ app/server.py
@@ -6,6 +6,7 @@ from mayim.extension import SanicMayimExtension
 from models.institute import Institute
 from models.subject import Subject
 from executors import InstituteExecutor
+from validator import validate_is_universal_param
 from typing import List
 import os
 
@@ -31,34 +32,30 @@ async def get_institutes(request: Request, executor: InstituteExecutor):
         institutes = await executor.get_institutes()
         return json({"institutes": [institute.__dict__ for institute in institutes]} , ensure_ascii=False, default=str)
     except Exception as e:
-        return exceptions.ServerError(f"{e}")
+        raise exceptions.ServerError(f"{e}")
 
-@app.get("/institutes/subjects/<institute_id>")
+@app.get("/institutes/<institute_id>/subjects")
 @openapi.response(List[Subject])
 async def get_subjects_by_institute_id(request: Request, institute_id: str,  executor: InstituteExecutor):
     try:
-        subjects = await executor.get_subjects_by_institute_id(institute_id)
-        return json({"subjects": [subject.__dict__ for subject in subjects]} , ensure_ascii=False, default=str)
-    except Exception as e:
-        return exceptions.ServerError(f"{e}")
+        is_universal = request.args.get("isUniversal", default = "false").lower()
+        validate_is_universal_param(is_universal)
 
-@app.get("/institutes/universal-subjects/<institute_id>")
-@openapi.response(List[Subject])
-async def get_universal_subjects_by_institute_id(request: Request, institute_id: str,  executor: InstituteExecutor):
-    try:
-        subjects = await executor.get__universal_subjects_by_institute_id(institute_id)
+        subjects = await executor.get_subjects_by_institute_id(institute_id, is_universal)
         return json({"subjects": [subject.__dict__ for subject in subjects]} , ensure_ascii=False, default=str)
+    except ValueError as e:
+        raise exceptions.BadRequest(f"{e}")
     except Exception as e:
-        return exceptions.ServerError(f"{e}")
+        raise exceptions.ServerError(f"{e}")
 
-@app.get("/course/subjects/<course_id>")
+@app.get("/course/<course_id>/subjects/")
 @openapi.response(List[Subject])
 async def get_subjects_by_course_id(request: Request, course_id: str,  executor: InstituteExecutor):
     try:
         subjects = await executor.get_subjects_by_course_id(course_id)
         return json({"subjects": [subject.__dict__ for subject in subjects]} , ensure_ascii=False, default=str)
     except Exception as e:
-        return exceptions.ServerError(f"{e}")
+        raise exceptions.ServerError(f"{e}")
 
 @app.get("subject/<subject_id>")
 async def get_subject_by_id(request: Request, subject_id: str,  executor: InstituteExecutor):
@@ -66,7 +63,7 @@ async def get_subject_by_id(request: Request, subject_id: str,  executor: Instit
         subject = await executor.get_subject_by_id(subject_id)
         return json( subject.__dict__ , ensure_ascii=False, default=str)
     except Exception as e:
-        return exceptions.ServerError(f"{e}")
+        raise exceptions.ServerError(f"{e}")
     
 if __name__ == '__main__':
     app.run(host="app", port=8000)
