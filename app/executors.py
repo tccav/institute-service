4374d3beacb5cb0ec2f55bceafb85e3de060c619
andressa.magalhaes <andressa.ribeiro@wp090383.local>
2023-07-13 19:16:19 -0300

    feat: update course subjects adding type and period in response

--- app/executors.py
+++ app/executors.py
@@ -1,6 +1,6 @@
 from mayim import PostgresExecutor, query
 from models.institute import Institute
-from models.subject import Subject
+from models.subject import Subject, CourseSubject
 from typing import List
 
 class InstituteExecutor(PostgresExecutor):
@@ -18,15 +18,16 @@ class InstituteExecutor(PostgresExecutor):
     GROUP BY subject_id
     )
     SELECT id, name, credits, workload_hours_total, workload_hours_per_week, is_universal, permits_agenda_conflict,
-        permits_preparation_situation, credits_requirements, approval_type, required_subjects
+        permits_preparation_situation, credits_requirements, approval_type, required_subjects, cs.type as type, cs.period as period
     FROM subjects as s
     LEFT JOIN courses_subjects as cs
     ON s.id = cs.subject_id
     LEFT JOIN required_subjects as rs
     ON s.id = rs.subject_id
     WHERE cs.course_id = $course_id
+    ORDER BY cs.period ASC, id ASC
     """)
-    async def get_subjects_by_course_id(self, course_id) -> List[Subject]:
+    async def get_subjects_by_course_id(self, course_id) -> List[CourseSubject]:
         ...
     
     @query("""
